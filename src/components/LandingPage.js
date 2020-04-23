4496f065ea150108d67a693c21229d2acc1c06aa
Ferdy Macleod <ferdymacleod21@gmail.com>
2020-04-23 17:57:33 +1000

    hopefully fix landing page styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,8 +8,10 @@ const Main = () => {
     return (
         <Fragment>
             <div className="main">
-                <h2>Not Just A Creative Software Developer</h2>
-                <h3>Lets Turn Problems Into Solutions</h3>
+                <div className="main-container">
+                    <h2>Not Just A Creative Software Developer</h2>
+                    <h3>Lets Turn Problems Into Solutions</h3>
+                </div>
                 <img className="down-arrow" src={downArrow} alt="profile of me"/>
             </div>
             <Intro />
